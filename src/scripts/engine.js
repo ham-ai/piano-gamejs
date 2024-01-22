3d82c48fbf193775e9e15f8ed23c55da84fda409
abraham <abrahamluccas13@gmail.com>
2024-01-22 17:39:28 -0300

    toggle keys on/off

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,18 +1,42 @@
 const pianokeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input")
 
+let mapedKeys = []
 let audio  = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
 };
 
 pianokeys.forEach((key) => {
     console.log(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key))
+    mapedKeys.push(key.dataset.key);
 });
 
 // keydown event 
 document.addEventListener("keydown", (e) => {
-    playTune(e.key);
-})
+    if (mapedKeys.includes(e.key)) {
+        playTune(e.key);
+    }   
+});
+
+const showHideKeys = () =>{
+    pianokeys.forEach(key => key.classList.toggle("hide"))
+}
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
